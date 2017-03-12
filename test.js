fef67c03fc93274b04abb92aa87a02bdf118efcc
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-03-12 20:30:13 +0700

    use assert.deepEqual instead assert.equal()

--- test.js
+++ test.js
@@ -2,9 +2,10 @@ const assert = require('assert')
 const getYears = require('./index')
 
 describe('index test', () => {
-  it('should return 2016, 2017, 2018', () => {
-    var actual = getYears('1');
-    var expected = [2016, 2017, 2018];
-    assert.equal(expected, actual);
-  });
+  it("should return 2016, 2017, 2018", function () {
+    const actual = getYears(1)
+    const expected = [2016, 2017, 2018];
+
+    assert.deepEqual(expected, actual)
+  })
 });
